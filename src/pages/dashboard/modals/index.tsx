702f15a73d74fe1aa1a71eefcaa2c741b0764769
Võ Đình Nghĩa <56909768+VoDinhNghia@users.noreply.github.com>
2023-11-05 19:50:54 +0700

    dev into main (#3)
    * config multi language and add menu page, footer page into home page
    
    * update header menu and add css into dialog of user module

--- src/pages/dashboard/modals/index.tsx
+++ src/pages/dashboard/modals/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./index.css";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { IpropModal } from "../../../interfaces/dashboard.interface";
