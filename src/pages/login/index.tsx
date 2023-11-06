5232e8b2b5350746786ec33986943cff4b35ddcf
Võ Đình Nghĩa <56909768+VoDinhNghia@users.noreply.github.com>
2023-11-06 14:53:33 +0700

    Dev (#5)
    * config multi language and add menu page, footer page into home page
    
    * update header menu and add css into dialog of user module
    
    * init modal user
    
    * update UI login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -14,6 +14,8 @@ import {
 } from "../../utils/login.util";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import MenuHome from "../menu-home";
+import FooterPage from "../commons/footer";
 
 const LoginPage = () => {
   const {
@@ -51,52 +53,66 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="LoginPage">
-      <Row>
-        <Col xl={6}>
-          <img src="/images/store-login.png" className="ImgLoginPage" />
-        </Col>
-        <Col xl={6} className="p-4">
-          <p className="text-center"><img src="/images/icon-login.jpg" alt="" className="IconLoginPage" /></p>
-          <h3 className="text-center">Login</h3>
-          <div className="p-4">
-            <form onSubmit={handleSubmit(onSubmitHandlerLogin)}>
-              <p className="mt-3">Email: </p>
-              <TextField
-                fullWidth={true}
-                size="small"
-                type="email"
-                className="border border-white"
-                error={!!errors["email"]}
-                helperText={errors["email"] ? errors["email"].message : ""}
-                {...register("email")}
+    <div>
+      <MenuHome numberCart={0} />
+      <div className="LoginPage">
+        <Row>
+          <Col xl={6}>
+            <img
+              src="/images/store-login.png"
+              className="ImgLoginPage"
+              height="420"
+            />
+          </Col>
+          <Col xl={6} className="p-4">
+            <p className="text-center">
+              <img
+                src="/images/icon-login.jpg"
+                alt=""
+                className="IconLoginPage"
               />
-              <p className="mt-2">Password: </p>
-              <TextField
-                fullWidth={true}
-                size="small"
-                type="password"
-                className="border border-white"
-                error={!!errors["password"]}
-                helperText={
-                  errors["password"] ? errors["password"].message : ""
-                }
-                {...register("password")}
-              />
-              <Button
-                type="submit"
-                variant="light"
-                className="mt-3 mb-2 w-100 text-primary fs-5"
-              >
-                Login
-              </Button>
-            </form>
-            <a href="/" className="text-decoration-none text-white">
-              Forget password ?
-            </a>
-          </div>
-        </Col>
-      </Row>
+            </p>
+            <h3 className="text-center">Login</h3>
+            <div className="p-4">
+              <form onSubmit={handleSubmit(onSubmitHandlerLogin)}>
+                <p className="mt-3">Email: </p>
+                <TextField
+                  fullWidth={true}
+                  size="small"
+                  type="email"
+                  className="border border-white"
+                  error={!!errors["email"]}
+                  helperText={errors["email"] ? errors["email"].message : ""}
+                  {...register("email")}
+                />
+                <p className="mt-2">Password: </p>
+                <TextField
+                  fullWidth={true}
+                  size="small"
+                  type="password"
+                  className="border border-white"
+                  error={!!errors["password"]}
+                  helperText={
+                    errors["password"] ? errors["password"].message : ""
+                  }
+                  {...register("password")}
+                />
+                <Button
+                  type="submit"
+                  variant="light"
+                  className="mt-3 mb-2 w-100 text-primary fs-5"
+                >
+                  Login
+                </Button>
+              </form>
+              <a href="/" className="text-decoration-none text-white">
+                Forget password ?
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </div>
+      <FooterPage />
     </div>
   );
 };
