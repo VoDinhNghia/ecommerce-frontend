c9a261d2b9fd63974cdf9d6aafa19d5886bb39ef
VoDinhNghia <vodinhnghia85@gmail.com>
2023-11-07 19:15:38 +0700

    write UI modal category

--- src/pages/dashboard/categories/modals/index.tsx
+++ src/pages/dashboard/categories/modals/index.tsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DialogModalCommonPage from "../../../commons/dialog-mui";
 import { connect } from "react-redux";
+import { useForm } from "react-hook-form";
 import { ImodalCategory } from "../../../../interfaces/category.interface";
-import { modalTypes } from "../../../../constants/constant";
+import { inputTypes, modalTypes } from "../../../../constants/constant";
 import { Button } from "@mui/material";
+import TextFieldCommon from "../../../commons/textfield-input";
 
 const ModalCategory = (props: ImodalCategory) => {
   const { isShowModal, onCloseModal, type, categoryInfo } = props;
+  const {
+    register,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm();
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+    reset(categoryInfo);
+  }, [isSubmitSuccessful, categoryInfo]);
 
   const content = (
     <div>
       {type === modalTypes.ADD || type === modalTypes.UPDATE ? (
         <form>
+          <p>Name: </p>
+          <TextFieldCommon
+            field="name"
+            register={register}
+            errors={errors}
+            defaultValue={categoryInfo?.name || ""}
+          />
+          <p className="mt-2">Description: </p>
+          <TextFieldCommon
+            field="description"
+            type={inputTypes.TEXT_AREA}
+            rows={6}
+            register={register}
+            errors={errors}
+            defaultValue={categoryInfo?.description || ""}
+          />
           <Button variant="contained" className="w-100 mt-3" type="submit">
             Save
           </Button>
