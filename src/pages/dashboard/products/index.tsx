d7ba8803d050bd53fd6a04450319a71bb62b4193
VoDinhNghia <vodinhnghia85@gmail.com>
2023-11-14 19:41:33 +0700

    show data product on table

--- src/pages/dashboard/products/index.tsx
+++ src/pages/dashboard/products/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "rsuite";
 import MenuPage from "../../commons/menu";
 import FooterPage from "../../commons/footer";
@@ -11,15 +11,56 @@ import {
   Table,
   TableBody,
   TableRow,
-  TableHead,
+  TableCell,
 } from "@mui/material";
 import { connect } from "react-redux";
-import { IpropProductPage } from "../../../interfaces/product.inteface";
+import {
+  Iproduct,
+  IpropProductPage,
+} from "../../../interfaces/product.inteface";
 import { IstateRedux } from "../../../interfaces/common.interface";
+import HeaderTableCommon from "../../commons/header-table";
+import { headerProductTable } from "../../../utils/product.util";
+import { Button } from "react-bootstrap";
+import ActionTableCommon from "../../commons/actions-table";
+import { productActions } from "../../../store/actions";
+import PaginationTableCommon from "../../commons/pagination-table";
 
 const ProductMgtPage = (props: IpropProductPage) => {
   const { dispatch, listProducts = [], totalProduct = 0 } = props;
+  const [state, setState] = useState({
+    page: 0,
+    limit: 5,
+    isShowModalAdd: false,
+    rowData: {},
+    isShowModalUpdate: false,
+    isShowModalDelete: false,
+  });
   const isRoleSa = validateRoleSa();
+  const { page, limit } = state;
+
+  const fetchProducts = (page: number, limit: number) => {
+    dispatch({
+      type: productActions.GET_LIST_PRODUCT,
+      payload: {
+        page,
+        limit,
+      },
+    });
+  };
+
+  const onSearch = (searchKey: string) => {
+    dispatch({
+      type: productActions.GET_LIST_PRODUCT,
+      payload: {
+        searchKey,
+      },
+    });
+  };
+
+  useEffect(() => {
+    fetchProducts(page + 1, limit);
+  }, []);
 
   return (
     <div>
@@ -29,8 +70,81 @@ const ProductMgtPage = (props: IpropProductPage) => {
             <MenuPage />
             <Container className="p-3">
               <TitleHeaderPage title="Products Management" />
-              <AddAndSearchTable title="Add product" />
-              <TableContainer></TableContainer>
+              <AddAndSearchTable
+                title="Add product"
+                onSearch={(key: string) => onSearch(key)}
+                onShowAdd={() => setState({ ...state, isShowModalAdd: true })}
+              />
+              <TableContainer>
+                <Table stickyHeader aria-label="product table">
+                  <HeaderTableCommon headerList={headerProductTable} />
+                  <TableBody>
+                    {listProducts?.map((product: Iproduct, index: number) => {
+                      return (
+                        <TableRow
+                          hover
+                          role="checkbox"
+                          tabIndex={-1}
+                          key={product?.id}
+                        >
+                          <TableCell>{index + 1}</TableCell>
+                          <TableCell>{product?.name}</TableCell>
+                          <TableCell>{product?.description}</TableCell>
+                          <TableCell>{product?.price}</TableCell>
+                          <TableCell>{product?.quantity}</TableCell>
+                          <TableCell>
+                            <Button variant="outline-primary" size="sm">
+                              View
+                            </Button>
+                          </TableCell>
+                          <TableCell>
+                            <Button variant="outline-primary" size="sm">
+                              View
+                            </Button>
+                          </TableCell>
+                          <TableCell>
+                            <Button variant="outline-primary" size="sm">
+                              View
+                            </Button>
+                          </TableCell>
+                          <TableCell>
+                            <Button variant="outline-primary" size="sm">
+                              View
+                            </Button>
+                          </TableCell>
+                          <TableCell>
+                            <Button variant="outline-primary" size="sm">
+                              View
+                            </Button>
+                          </TableCell>
+                          <TableCell>
+                            <Button variant="outline-primary" size="sm">
+                              View
+                            </Button>
+                          </TableCell>
+                          <TableCell>
+                            <ActionTableCommon
+                              state={state}
+                              setState={setState}
+                              rowData={product}
+                            />
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <PaginationTableCommon
+                total={totalProduct}
+                limit={limit}
+                page={page}
+                setState={setState}
+                state={state}
+                fetchList={(page: number, limit: number) =>
+                  fetchProducts(page, limit)
+                }
+              />
             </Container>
           </Container>
           <FooterPage />
