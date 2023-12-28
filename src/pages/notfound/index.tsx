6a4aa487103932f4732852a72d0d9e57d9dd5c8b
VoDinhNghia <vodinhnghia85@gmail.com>
2023-12-28 18:37:53 +0700

    refactor UI page not found

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./index.css";
-import { routes } from "../../constants/constant";
+import MenuHome from "../menu-home";
+import { getCart } from "../../services/cart.service";
+import Footer from "../commons/footer";
 
 const NotfoundPage = () => {
   return (
-    <div className="NotFoundPage fs-6">
-      <p>
-        <a href={routes.home} className="fs-5">
-          Go to homepage
-        </a>
-      </p>
-      <img src="/images/notfound.jpg" width="100%" />
-    </div>
+    <>
+      <MenuHome numberCart={getCart()?.length || 0} />
+      <div className="NotFoundPage fs-6 container">
+        <img src="/images/notfound.jpg" width="100%" height="400px" />
+      </div>
+      <Footer />
+    </>
   );
 };
 
