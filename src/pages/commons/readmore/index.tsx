85bf8a89d16556c743fee6abd3e9581e83ce07d9
VoDinhNghia <vodinhnghia85@gmail.com>
2024-01-23 19:39:50 +0700

    refactor readmore common page

--- src/pages/commons/readmore/index.tsx
+++ src/pages/commons/readmore/index.tsx
@@ -4,30 +4,19 @@ import { Button } from "react-bootstrap";
 import { IpropReadMore } from "../../../interfaces/common.interface";
 
 const ReadMoreCommon = (props: IpropReadMore) => {
-  const {
-    children,
-    isReadMore,
-    setReadMore,
-    lengthSlice = 30,
-  } = props;
+  const { children, isReadMore, setReadMore, lengthSlice = 30 } = props;
   const text = children;
 
   return (
     <p className="ReadMoreCommon">
       {!isReadMore && text?.length > lengthSlice
-        ? text?.slice(0, lengthSlice)
+        ? `${text?.slice(0, lengthSlice)}...`
         : text}
       <span onClick={() => setReadMore()}>
-        {text && text?.length > lengthSlice ? (
-          !isReadMore ? (
-            <Button size="sm" variant="outline-light" className="text-primary">
-              read more
-            </Button>
-          ) : (
-            <Button size="sm" variant="outline-light" className="text-primary">
-              show less
-            </Button>
-          )
+        {text.length > lengthSlice ? (
+          <Button size="sm" variant="outline-light" className="text-primary">
+            {!isReadMore ? "read more" : "show less"}
+          </Button>
         ) : null}
       </span>
     </p>
