a7ebc30095ebab8bb16c2d6530e0f906705811cb
VoDinhNghia <vodinhnghia85@gmail.com>
2024-01-24 08:12:57 +0700

    refactor modal user profile mgt page on dashboard

--- src/pages/dashboard/modals/index.tsx
+++ src/pages/dashboard/modals/index.tsx
@@ -57,8 +57,106 @@ const DashboardModalPage = (props: IpropModal) => {
   const onHandleSubmitUpdatePassword: SubmitHandler<
     IregisterInputUpdatePassordForm
   > = (values) => {
-    console.log('values', values);
+    console.log("values", values);
   };
+
+  const FormUpdatePassword = (
+    <form onSubmit={handleSubmit(onHandleSubmitUpdatePassword)}>
+      <p className="mb-1">Current password: </p>
+      <TextFieldCommon
+        field="currentPassword"
+        type={inputTypes.PASSWORD}
+        errors={errors}
+        register={register}
+      />
+      <p className="mt-2 mb-1">New password: </p>
+      <TextFieldCommon
+        field="newPassword"
+        type={inputTypes.PASSWORD}
+        errors={errors}
+        register={register}
+      />
+      <p className="mt-2 mb-1">Enter new password: </p>
+      <TextFieldCommon
+        field="confirmPassword"
+        type={inputTypes.PASSWORD}
+        errors={errors}
+        register={register}
+      />
+      <Button type="submit" variant="contained" className="mt-4 w-100">
+        Change password
+      </Button>
+    </form>
+  );
+
+  const UpdateForm = (
+    <>
+      <p className="mb-1">Email: </p>
+      <TextField
+        size="small"
+        variant="outlined"
+        defaultValue={userInfo?.email}
+        fullWidth={true}
+        onChange={(e: IeventOnchangeInput) =>
+          setState({ ...state, email: e.target.value })
+        }
+      />
+    </>
+  );
+
+  const UpdateProfileContent = (
+    <>
+      <p className="mb-1">LastName: </p>
+      <TextField
+        size="small"
+        variant="outlined"
+        defaultValue={userInfo?.profile?.lastName}
+        fullWidth={true}
+        onChange={(e: IeventOnchangeInput) =>
+          setState({ ...state, lastName: e.target.value })
+        }
+      />
+      <p className="mt-2 mb-1">MiddleName: </p>
+      <TextField
+        size="small"
+        variant="outlined"
+        defaultValue={userInfo?.profile?.middleName}
+        fullWidth={true}
+        onChange={(e: IeventOnchangeInput) =>
+          setState({ ...state, middleName: e.target.value })
+        }
+      />
+      <p className="mt-2 mb-1">FirstName: </p>
+      <TextField
+        size="small"
+        variant="outlined"
+        defaultValue={userInfo?.profile?.firstName}
+        fullWidth={true}
+        onChange={(e: IeventOnchangeInput) =>
+          setState({ ...state, firstName: e.target.value })
+        }
+      />
+      <p className="mt-2 mb-1">Gender: </p>
+      <SelectMuiCommon
+        type={selectMuiTypes.NORMAL}
+        options={userGenderOptions}
+        defaultValue={userInfo?.profile?.gender}
+        onChangeSelect={(value: string) =>
+          setState({ ...state, gender: value })
+        }
+      />
+      <p className="mt-2 mb-1">Mobile: </p>
+      <TextField
+        size="small"
+        variant="outlined"
+        defaultValue={userInfo?.profile?.mobile}
+        fullWidth={true}
+        onChange={(e: IeventOnchangeInput) =>
+          setState({ ...state, mobile: e.target.value })
+        }
+      />
+    </>
+  );
 
   return (
     <Dialog
@@ -76,116 +174,18 @@ const DashboardModalPage = (props: IpropModal) => {
         </IconButton>
       </DialogTitle>
       <DialogContent>
-        {type === modalTypes.UPDATE ? (
-          <>
-            <p className="mb-1">Email: </p>
-            <TextField
-              size="small"
-              variant="outlined"
-              defaultValue={userInfo?.email}
-              fullWidth={true}
-              onChange={(e: IeventOnchangeInput) =>
-                setState({ ...state, email: e.target.value })
-              }
-            />
-          </>
-        ) : null}
-        {type === modalTypes.UPDATE_PASSWORD ? (
-          <form onSubmit={handleSubmit(onHandleSubmitUpdatePassword)}>
-            <p className="mb-1">Current password: </p>
-            <TextFieldCommon
-              field="currentPassword"
-              type={inputTypes.PASSWORD}
-              errors={errors}
-              register={register}
-            />
-            <p className="mt-2 mb-1">New password: </p>
-            <TextFieldCommon
-              field="newPassword"
-              type={inputTypes.PASSWORD}
-              errors={errors}
-              register={register}
-            />
-            <p className="mt-2 mb-1">Enter new password: </p>
-            <TextFieldCommon
-              field="confirmPassword"
-              type={inputTypes.PASSWORD}
-              errors={errors}
-              register={register}
-            />
-            <Button type="submit" variant="contained" className="mt-4 w-100">
-              Change password
-            </Button>
-          </form>
-        ) : null}
-        {type === modalTypes.UPDATE_PROFILE ? (
-          <>
-            <p className="mb-1">LastName: </p>
-            <TextField
-              size="small"
-              variant="outlined"
-              defaultValue={userInfo?.profile?.lastName}
-              fullWidth={true}
-              onChange={(e: IeventOnchangeInput) =>
-                setState({ ...state, lastName: e.target.value })
-              }
-            />
-            <p className="mt-2 mb-1">MiddleName: </p>
-            <TextField
-              size="small"
-              variant="outlined"
-              defaultValue={userInfo?.profile?.middleName}
-              fullWidth={true}
-              onChange={(e: IeventOnchangeInput) =>
-                setState({ ...state, middleName: e.target.value })
-              }
-            />
-            <p className="mt-2 mb-1">FirstName: </p>
-            <TextField
-              size="small"
-              variant="outlined"
-              defaultValue={userInfo?.profile?.firstName}
-              fullWidth={true}
-              onChange={(e: IeventOnchangeInput) =>
-                setState({ ...state, firstName: e.target.value })
-              }
-            />
-            <p className="mt-2 mb-1">Gender: </p>
-            <SelectMuiCommon
-              type={selectMuiTypes.NORMAL}
-              options={userGenderOptions}
-              defaultValue={userInfo?.profile?.gender}
-              onChangeSelect={(value: string) =>
-                setState({ ...state, gender: value })
-              }
-            />
-            <p className="mt-2 mb-1">Mobile: </p>
-            <TextField
-              size="small"
-              variant="outlined"
-              defaultValue={userInfo?.profile?.mobile}
-              fullWidth={true}
-              onChange={(e: IeventOnchangeInput) =>
-                setState({ ...state, mobile: e.target.value })
-              }
-            />
-          </>
-        ) : null}
+        {type === modalTypes.UPDATE ? UpdateForm : null}
+        {type === modalTypes.UPDATE_PASSWORD ? FormUpdatePassword : null}
+        {type === modalTypes.UPDATE_PROFILE ? UpdateProfileContent : null}
       </DialogContent>
       <DialogActions>
         {type === modalTypes.UPDATE ? (
-          <Button
-            variant="contained"
-            className="w-100"
-          >
+          <Button variant="contained" className="w-100">
             Save Info
           </Button>
         ) : null}
         {type === modalTypes.UPDATE_PROFILE ? (
-          <Button
-            variant="contained"
-            className="w-100"
-          >
+          <Button variant="contained" className="w-100">
             Save Profile
           </Button>
         ) : null}
