c402eda5e213283ae60eefc87cd9cd66c5900f07
VoDinhNghia <vodinhnghia85@gmail.com>
2024-01-24 08:31:09 +0700

    refactor carts page

--- src/pages/carts/index.tsx
+++ src/pages/carts/index.tsx
@@ -66,17 +66,12 @@ const CartDetailPage = (props: IpropCartDetailPage) => {
       addTocart(product, numberQuantity > 0 ? numberQuantity : 1);
       NotificationManager.success("Add to cart success", "Add to cart", 3000);
     }
-    setTimeout(() => {
-      fetchCarts();
-    }, 70);
-    setState({ ...state, numberQuantity: 0 });
+    fetchAndSetQuantity();
   };
 
   const subtractCart = (product: Iproduct) => {
     removeCart(product, numberQuantity > 0 ? numberQuantity : 1);
-    setTimeout(() => {
-      fetchCarts();
-    }, 70);
+    fetchAndSetQuantity();
     NotificationManager.success(
       `${
         product?.quantity - 1 === 0 ? "remove" : "subtract quantity"
@@ -84,10 +79,13 @@ const CartDetailPage = (props: IpropCartDetailPage) => {
       "Subtract cart",
       3000
     );
-    setState({
-      ...state,
-      numberQuantity: 0,
-    });
+  };
+
+  const fetchAndSetQuantity = () => {
+    setTimeout(() => {
+      fetchCarts();
+    }, 70);
+    setState({ ...state, numberQuantity: 0 });
   };
 
   const clearCarts = (product: Iproduct) => {
@@ -104,6 +102,119 @@ const CartDetailPage = (props: IpropCartDetailPage) => {
     fetchProducts();
   }, []);
 
+  const ProductsItem = (
+    <Col xl={7} className="px-5 py-4">
+      <MDBTypography
+        tag="h3"
+        className="mb-5 pt-2 text-center fw-bold text-uppercase"
+      >
+        Your products
+      </MDBTypography>
+
+      {carts?.map((cart: Iproduct) => {
+        return (
+          <div className="d-flex align-items-center mb-5" key={cart?.id}>
+            <div className="flex-shrink-0">
+              <MDBCardImage
+                src={cart?.images}
+                fluid
+                style={{ width: "150px" }}
+                alt="Generic placeholder image"
+              />
+            </div>
+
+            <div className="flex-grow-1 ms-3">
+              <Button
+                className="float-end"
+                size="sm"
+                variant="outline-danger"
+                onClick={() => clearCarts(cart)}
+              >
+                <MDBIcon fas icon="times" />
+              </Button>
+              <MDBTypography tag="h5" className="text-primary">
+                {cart?.name}
+              </MDBTypography>
+              <MDBTypography tag="h6">Quantity: {cart?.quantity}</MDBTypography>
+
+              <div className="d-flex align-items-center">
+                <p className="fs-6 fw-bold mb-0 me-5 pe-3 text-danger">
+                  {cart?.price?.toLocaleString("en-US")} đ
+                </p>
+
+                <div className="def-number-input number-input safari_only">
+                  <button
+                    className="minus"
+                    onClick={() => subtractCart(cart)}
+                  ></button>
+                  <input
+                    className="quantity fw-bold text-black"
+                    min={0}
+                    defaultValue={1}
+                    type="number"
+                    onChange={(e: IformAddToCart) =>
+                      setState({
+                        ...state,
+                        numberQuantity: Number(e?.target?.value),
+                      })
+                    }
+                  />
+                  <button
+                    className="plus"
+                    onClick={() => addCart(cart)}
+                  ></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+
+      <hr
+        className="mb-4"
+        style={{
+          height: "2px",
+          backgroundColor: "#1266f1",
+          opacity: 1,
+        }}
+      />
+      <div
+        className="d-flex justify-content-between p-2 mb-2"
+        style={{ backgroundColor: "#e1f5fe" }}
+      >
+        <MDBTypography tag="h5" className="fw-bold mb-0">
+          Total:
+        </MDBTypography>
+        <MDBTypography tag="h5" className="fw-bold mb-0">
+          {totalPrice?.toLocaleString("en-US")} đ
+        </MDBTypography>
+      </div>
+    </Col>
+  );
+
+  const PaymentContent = (
+    <Col xl={5} className="px-5 py-4">
+      <MDBTypography
+        tag="h3"
+        className="mb-5 pt-2 text-center fw-bold text-uppercase"
+      >
+        Payment
+      </MDBTypography>
+      <form className="mb-5">
+        <MDBTypography
+          tag="h5"
+          className="fw-bold mb-5"
+          style={{ position: "absolute", bottom: "0" }}
+        >
+          <a href={routes.home}>
+            <MDBIcon fas icon="angle-left me-2" />
+            Back to shopping
+          </a>
+        </MDBTypography>
+      </form>
+    </Col>
+  );
+
   return (
     <div>
       <MenuHomePage numberCart={numberCart} />
@@ -117,120 +228,8 @@ const CartDetailPage = (props: IpropCartDetailPage) => {
               >
                 <MDBCardBody className="text-black">
                   <MDBRow>
-                    <Col xl={7} className="px-5 py-4">
-                      <MDBTypography
-                        tag="h3"
-                        className="mb-5 pt-2 text-center fw-bold text-uppercase"
-                      >
-                        Your products
-                      </MDBTypography>
-
-                      {carts?.map((cart: Iproduct) => {
-                        return (
-                          <div
-                            className="d-flex align-items-center mb-5"
-                            key={cart?.id}
-                          >
-                            <div className="flex-shrink-0">
-                              <MDBCardImage
-                                src={cart?.images}
-                                fluid
-                                style={{ width: "150px" }}
-                                alt="Generic placeholder image"
-                              />
-                            </div>
-
-                            <div className="flex-grow-1 ms-3">
-                              <Button
-                                className="float-end"
-                                size="sm"
-                                variant="outline-danger"
-                                onClick={() => clearCarts(cart)}
-                              >
-                                <MDBIcon fas icon="times" />
-                              </Button>
-                              <MDBTypography tag="h5" className="text-primary">
-                                {cart?.name}
-                              </MDBTypography>
-                              <MDBTypography tag="h6">
-                                Quantity: {cart?.quantity}
-                              </MDBTypography>
-
-                              <div className="d-flex align-items-center">
-                                <p className="fs-6 fw-bold mb-0 me-5 pe-3 text-danger">
-                                  {cart?.price?.toLocaleString("en-US")} đ
-                                </p>
-
-                                <div className="def-number-input number-input safari_only">
-                                  <button
-                                    className="minus"
-                                    onClick={() => subtractCart(cart)}
-                                  ></button>
-                                  <input
-                                    className="quantity fw-bold text-black"
-                                    min={0}
-                                    defaultValue={1}
-                                    type="number"
-                                    onChange={(e: IformAddToCart) =>
-                                      setState({
-                                        ...state,
-                                        numberQuantity: Number(
-                                          e?.target?.value
-                                        ),
-                                      })
-                                    }
-                                  />
-                                  <button
-                                    className="plus"
-                                    onClick={() => addCart(cart)}
-                                  ></button>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        );
-                      })}
-
-                      <hr
-                        className="mb-4"
-                        style={{
-                          height: "2px",
-                          backgroundColor: "#1266f1",
-                          opacity: 1,
-                        }}
-                      />
-                      <div
-                        className="d-flex justify-content-between p-2 mb-2"
-                        style={{ backgroundColor: "#e1f5fe" }}
-                      >
-                        <MDBTypography tag="h5" className="fw-bold mb-0">
-                          Total:
-                        </MDBTypography>
-                        <MDBTypography tag="h5" className="fw-bold mb-0">
-                          {totalPrice?.toLocaleString("en-US")} đ
-                        </MDBTypography>
-                      </div>
-                    </Col>
-                    <Col xl={5} className="px-5 py-4">
-                      <MDBTypography
-                        tag="h3"
-                        className="mb-5 pt-2 text-center fw-bold text-uppercase"
-                      >
-                        Payment
-                      </MDBTypography>
-                      <form className="mb-5">
-                        <MDBTypography
-                          tag="h5"
-                          className="fw-bold mb-5"
-                          style={{ position: "absolute", bottom: "0" }}
-                        >
-                          <a href={routes.home}>
-                            <MDBIcon fas icon="angle-left me-2" />
-                            Back to shopping
-                          </a>
-                        </MDBTypography>
-                      </form>
-                    </Col>
+                    {ProductsItem}
+                    {PaymentContent}
                   </MDBRow>
                 </MDBCardBody>
               </MDBCard>
